e5f7ab371b50dc10f0f82ade2bb4936b916c00f4
Asmit <ashmits554@gmail.com>
2026-01-04 23:41:49 +0530

    Refactor AdminHome component layout and structure
    Updated layout and structure of the Admin Home component, including profile and info sections.

--- src/Components/Admin/AdminHome/AdminHome.jsx
+++ src/Components/Admin/AdminHome/AdminHome.jsx
@@ -1,52 +1,69 @@
-import React from 'react'
-import Breadcrum from '../../Partials/Breadcrum'
-import Sidebar from '../Sidebar'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Sidebar from "../Sidebar";
+import { Link } from "react-router-dom";
 
 export default function AdminHome() {
   return (
     <>
-      {/* <Breadcrum title="Admin"/>  */}
-      <div className="container-fluid my-3">
-        <div className="row">
-            <div className="col-xl-2 col-md-3">
-                <Sidebar/>
-            </div>
-            <div className="col-xl-10 col-md-9 ">
-                 <div className="row">
-                  <div className="col-md-6">
-                    <img src="/img/noimage.jpeg" height={450} width="100%" alt="" />
+      <div className="container-fluid my-4">
+        <div className="row g-4">
+          <div className="col-xl-2 col-md-3">
+            <Sidebar />
+          </div>
+
+          <div className="col-xl-10 col-md-9">
+            <div className="row g-4 align-items-center">
+
+              {/* Profile Section */}
+              <div className="col-md-4 text-center">
+                <div className="profile-avatar-wrapper mx-auto">
+                  <img
+                    src="/img/noimage.jpeg"
+                    alt="Admin"
+                    className="profile-avatar"
+                  />
+                </div>
+                <h5 className="mt-3 fw-semibold">Asmit</h5>
+                <span className="text-muted">Administrator</span>
+              </div>
+
+              {/* Info Section */}
+              <div className="col-md-8">
+                <div className="card shadow-sm border-0">
+                  <div className="card-header bg-success text-white fw-semibold">
+                    Admin Details
                   </div>
-                    <div className="col-md-6">
-                      <h5 className='custom-green text-center text-light p-2'>Admin Home Page</h5>
-                      <table className='table table-bordered'>
-                        <tbody>
-                          <tr>
-                            <th>Name</th>
-                            <td>Asmit</td>
-                          </tr>
-                          <tr>
-                            <th>User Name</th>
-                            <td>asmit</td>
-                          </tr>
-                          <tr>
-                            <th>Email</th>
-                            <td>ashmits554@gmail.com</td>
-                          </tr>
-                          <tr>
-                            <th>Phone</th>
-                            <td>9540503529</td>
-                          </tr>
-                          <tr>
-                            <td colSpan={2}><Link to="/update-profile" className="btn btn-primary w-100">Update Profile</Link></td>
-                          </tr>
-                        </tbody>
-                      </table>
+
+                  <div className="card-body">
+                    <div className="row mb-2">
+                      <div className="col-4 fw-semibold text-muted">Username</div>
+                      <div className="col-8">asmit</div>
+                    </div>
+
+                    <div className="row mb-2">
+                      <div className="col-4 fw-semibold text-muted">Email</div>
+                      <div className="col-8">ashmits554@gmail.com</div>
+                    </div>
+
+                    <div className="row mb-3">
+                      <div className="col-4 fw-semibold text-muted">Phone</div>
+                      <div className="col-8">9540503529</div>
+                    </div>
+
+                    <Link
+                      to="/update-profile"
+                      className="btn btn-primary rounded-pill px-4"
+                    >
+                      Update Profile
+                    </Link>
                   </div>
-                 </div>
+                </div>
+              </div>
+
             </div>
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
