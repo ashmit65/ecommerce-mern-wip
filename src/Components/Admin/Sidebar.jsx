6661211307886420a9e6c450b6576436dbf929fd
Asmit <ashmits554@gmail.com>
2026-01-12 23:33:46 +0530

    Refactor Sidebar component for improved structure

--- src/Components/Admin/Sidebar.jsx
+++ src/Components/Admin/Sidebar.jsx
@@ -1,94 +1,63 @@
 import React from "react";
-import Breadcrum from "../Partials/Breadcrum";
 import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
+  const linkClass = ({ isActive }) =>
+    `list-group-item sidebar-link d-flex align-items-center gap-3 ${
+      isActive ? "active" : ""
+    }`;
+
   return (
-    <>
-      <div className="list-group">
-        <NavLink
-          to="/admin"
-          className="list-group-item active mt-1 rounded-2"
-          aria-current="true"
-        >
-          <i className="fa fa-home"></i>
-          <span className="float-end">Home</span>
-        </NavLink>
-        
-        <NavLink
-          to="/admin/Maincategory"
-          className="list-group-item active mt-1 rounded-2"
-          aria-current="true"
-        >
-          <i className="fa fa-list"></i>
-          <span className="float-end">Maincategory</span>
-        </NavLink>
-        
-        <NavLink
-          to="/admin/Subcategory"
-          className="list-group-item active mt-1 rounded-2"
-          aria-current="true"
-        >
-          <i className="fa fa-list"></i>
-          <span className="float-end">Subcategory</span>
-        </NavLink>
-        <NavLink
-          to="/admin/Brand"
-          className="list-group-item active mt-1 rounded-2"
-          aria-current="true"
-        >
-          <i className="fa fa-list"></i>
-          <span className="float-end">Brand</span>
-        </NavLink>
-        <NavLink
-          to="/admin/Product"
-          className="list-group-item active mt-1 rounded-2"
-          aria-current="true"
-        >
-          <i className="fa fa-list"></i>
-          <span className="float-end">Product</span>
-        </NavLink>
-        <NavLink
-          to="/admin/Testimonial"
-          className="list-group-item active mt-1 rounded-2"
-          aria-current="true"
-        >
-          <i className="fa fa-star"></i>
-          <span className="float-end">Testimonial</span>
-        </NavLink>
-        <NavLink
-          to="/admin/Users"
-          className="list-group-item active mt-1 rounded-2"
-          aria-current="true"
-        >
-          <i className="fa fa-users"></i>
-          <span className="float-end">Users</span>
-        </NavLink>
-        <NavLink
-          to="/admin/Newsletter"
-          className="list-group-item active mt-1 rounded-2"
-          aria-current="true"
-        >
-          <i className="fa fa-envelope"></i>
-          <span className="float-end">Newsletter</span>
-        </NavLink>
-        <NavLink
-          to="/admin/Contactus"
-          className="list-group-item active mt-1 rounded-2"
-          aria-current="true"
-        >
-          <i className="fa fa-phone"></i>
-          <span className="float-end">Contactus</span>
-        </NavLink>
-        <NavLink
-          to="/admin/Checkouts"
-          className="list-group-item active mt-1 rounded-2"
-          aria-current="true"
-        >
-          <i className="fa fa-shopping-bag"></i>
-          <span className="float-end">Checkouts</span>
-        </NavLink>
-      </div>
-    </>
+    <div className="list-group sidebar shadow-sm rounded-4 p-3">
+      <NavLink to="/admin" className={linkClass}>
+        <i className="fa fa-home"></i>
+        <span>Dashboard</span>
+      </NavLink>
+
+      <NavLink to="/admin/Maincategory" className={linkClass}>
+        <i className="fa fa-layer-group"></i>
+        <span>Main Category</span>
+      </NavLink>
+
+      <NavLink to="/admin/Subcategory" className={linkClass}>
+        <i className="fa fa-list"></i>
+        <span>Sub Category</span>
+      </NavLink>
+
+      <NavLink to="/admin/Brand" className={linkClass}>
+        <i className="fa fa-tags"></i>
+        <span>Brands</span>
+      </NavLink>
+
+      <NavLink to="/admin/Product" className={linkClass}>
+        <i className="fa fa-box"></i>
+        <span>Products</span>
+      </NavLink>
+
+      <NavLink to="/admin/Testimonial" className={linkClass}>
+        <i className="fa fa-star"></i>
+        <span>Testimonials</span>
+      </NavLink>
+
+      <NavLink to="/admin/Users" className={linkClass}>
+        <i className="fa fa-users"></i>
+        <span>Users</span>
+      </NavLink>
+
+      <NavLink to="/admin/Newsletter" className={linkClass}>
+        <i className="fa fa-envelope"></i>
+        <span>Newsletter</span>
+      </NavLink>
+
+      <NavLink to="/admin/Contactus" className={linkClass}>
+        <i className="fa fa-phone"></i>
+        <span>Contact Us</span>
+      </NavLink>
+
+      <NavLink to="/admin/Checkouts" className={linkClass}>
+        <i className="fa fa-shopping-bag"></i>
+        <span>Checkouts</span>
+      </NavLink>
+    </div>
   );
 }
